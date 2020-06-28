e556edccb0acf8c943076c83075bc01983e90651
Tomás Davison <toomi.davison@gmail.com>
2020-06-28 14:42:18 -0300

    ADD - Studies component

---
+++ src/components/Studies/Studies.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import Title from '../Utils/Title'
+
+
+const Container = styled.div`
+    margin-left: 20px;
+    margin-top: 50px;
+`;
+
+const Studies = () => {
+    return (
+        <Container id="studies">
+            <Title title="Studies"/>
+        </Container>
+    )
+}
+
+export default Studies
